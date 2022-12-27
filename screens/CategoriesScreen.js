a8047c8bb5803096230603e0f041989d99161862
r.danna <rafdanna28@gmail.com>
2022-12-27 12:26:44 +0100

    aggiunta dei pasti alla seconda schermata

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,7 +6,9 @@ import CategoryGridTile from "../components/CategoryGridTile";
 function CategoriesScreen({navigation}){
     function renderCategoryItem(itemData){
         function pressHandler(){
-            navigation.navigate("MealsOverview");
+            navigation.navigate("MealsOverview", {
+                categoryId: itemData.item.id,
+            });
         }
 
         return(
