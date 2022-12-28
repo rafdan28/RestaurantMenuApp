1e7823605d92119616d05646a03edf8420ab1ef5
r.danna <rafdanna28@gmail.com>
2022-12-28 15:41:21 +0100

    navigation drawer style pt2

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {createDrawerNavigator} from "@react-navigation/drawer";
+import {Ionicons} from '@expo/vector-icons'
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
@@ -16,17 +17,28 @@ function DrawerNavigator(){
             screenOptions={{
                 headerStyle: {backgroundColor: '#351401'},
                 headerTintColor: 'white',
-                sceneContainerStyle: {backgroundColor: '#3f2f25'}
+                sceneContainerStyle: {backgroundColor: '#3f2f25'},
+                drawerContentStyle: {backgroundColor: '#351401'},
+                drawerInactiveTintColor: 'white',
+                drawerActiveTintColor: '#351401',
+                drawerActiveBackgroundColor: '#e4baa1'
             }}
         >
             <Drawer.Screen
                 name='Categories'
                 component={CategoriesScreen}
                 options={{
-                    title: "All Categories"
+                    title: "All Categories",
+                    drawerIcon: ({color, size}) => <Ionicons name="list" color={color} size={size}/>
                 }}
             />
-            <Drawer.Screen name='Favorites' component={FavoriteScreen}/>
+            <Drawer.Screen
+                name='Favorites'
+                component={FavoriteScreen}
+                options={{
+                    drawerIcon: ({color, size}) => <Ionicons name="star" color={color} size={size}/>
+                }}
+            />
         </Drawer.Navigator>
     );
 }
