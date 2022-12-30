b16f73bd6bccd00d4312ad40438993995ebca805
r.danna <rafdanna28@gmail.com>
2022-12-30 12:02:49 +0100

    settaggio di redux

--- App.js
+++ App.js
@@ -8,7 +8,9 @@ import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoriteScreen from "./screens/FavoriteScreen";
-import FavoriteContextProvider from "./store/context/favorite-context";
+// import FavoriteContextProvider from "./store/context/favorite-context";
+import {Provider} from "react-redux";
+import {store} from "./store/redux/store"
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -47,7 +49,8 @@ export default function App() {
   return (
       <>
           <StatusBar style='light'/>
-          <FavoriteContextProvider>
+          {/*<FavoriteContextProvider>*/}
+          <Provider store={store}>
               <NavigationContainer>
                   <Stack.Navigator
                       screenOptions={{
@@ -77,7 +80,8 @@ export default function App() {
                       />
                   </Stack.Navigator>
               </NavigationContainer>
-          </FavoriteContextProvider>
+          </Provider>
+          {/*</FavoriteContextProvider>*/}
       </>
 
   );
